3cdc941135dfc720d3576870e0392510e85945d6
justin22a <justin.adam@gmail.com>
2024-11-05 10:30:41 -0500

    pins on a map

--- gta-app/src/MapView.js
+++ gta-app/src/MapView.js
@@ -1,7 +1,25 @@
 // src/MapView.js
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// Define a custom icon for the markers
+const customIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+const sampleLocations = [
+    { username: "User1", latitude: 29.652, longitude: -82.325, description: "Stolen bike" },
+    { username: "User2", latitude: 29.653, longitude: -82.323, description: "Stolen scooter" },
+    { username: "User3", latitude: 29.654, longitude: -82.326, description: "Stolen laptop" },
+  ];
 
 const MapView = ({ theftLocations = [] }) => {
   const defaultPosition = [29.6516, -82.3248]; // Center on Gainesville, FL
@@ -12,8 +30,12 @@ const MapView = ({ theftLocations = [] }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {theftLocations.map((location, index) => (
-        <Marker key={index} position={[location.latitude, location.longitude]}>
+      {sampleLocations.map((location, index) => (
+        <Marker
+          key={index}
+          position={[location.latitude, location.longitude]}
+          icon={customIcon} // Use the custom icon here
+        >
           <Popup>
             <strong>{location.username}</strong>
             <p>{location.description}</p>
